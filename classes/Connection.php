bb9f82aba05b3951581ad10018c48ba39933c090
Ryangraves93 <ryangraves1993@gmail.com>
2021-01-27 15:02:41 +0000

    Intial commit

--- classes/Connection.php
+++ classes/Connection.php
@@ -1,10 +1,10 @@
 <?php
 class Connection {
 
-    private static $host = "localhost";
-    private static $database = "cloud_festivals_db";
-    private static $username = "root";
-    private static $password = "";
+    private static $host = "cloudfestivaldb.ctvhtpgh3tcu.us-east-1.rds.amazonaws.com";
+    private static $database = "CADB";
+    private static $username = "admin";
+    private static $password = "Ryangraves08";
 
     public static function getInstance() {
         $dsn = 'mysql:host=' . Connection::$host . ';dbname=' . Connection::$database;
